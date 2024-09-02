0b4b7406bf3bf5e3af667684b5bda70c7e2f3897
sebmartinezpedemonte <101581906+sebmartinezpedemonte@users.noreply.github.com>
2024-09-02 15:13:03 -0300

    added console log for debugging

--- force-app/main/default/lwc/imageControl/imageControl.js
+++ force-app/main/default/lwc/imageControl/imageControl.js
@@ -1,6 +1,10 @@
-import { LightningElement,api } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class ImageControl extends LightningElement {
     @api url;
     @api altText;
+
+    renderedCallback() {
+        console.log('Recibi altText en ImageControl:', this.altText);
+    }
 }
