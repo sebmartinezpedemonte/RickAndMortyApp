0e1bdeec17764fee1249509f90d1b585e6c8fdfa
sebmartinezpedemonte <101581906+sebmartinezpedemonte@users.noreply.github.com>
2024-09-02 16:09:16 -0300

    Changes to use Schema to define the columns

--- force-app/main/default/lwc/rickAndMortyCharactersList/rickAndMortyCharactersList.js
+++ force-app/main/default/lwc/rickAndMortyCharactersList/rickAndMortyCharactersList.js
@@ -1,6 +1,13 @@
 import { LightningElement, wire, track } from 'lwc';
 import getCharacters from '@salesforce/apex/CharactersController.getCharacters';
 import { NavigationMixin } from 'lightning/navigation';
+import FIELD_NAME from '@salesforce/schema/Personaje__c.Name';
+import FIELD_STATUS from '@salesforce/schema/Personaje__c.Status__c';
+import FIELD_SPECIES from '@salesforce/schema/Personaje__c.Species__c';
+import FIELD_GENDER from '@salesforce/schema/Personaje__c.Gender__c';
+import FIELD_IMAGE from '@salesforce/schema/Personaje__c.ImageUrl__c';
+import FIELD_URL from '@salesforce/schema/Personaje__c.Url__c';
+import FIELD_EXTID from '@salesforce/schema/Personaje__c.ExtId__c';
 
 export default class rickAndMortyCharactersList extends NavigationMixin(LightningElement) {
     @track characters = [];
@@ -8,20 +15,20 @@ export default class rickAndMortyCharactersList extends NavigationMixin(Lightnin
     @track sortedDirection = 'asc';
 
     columns = [
-        { label: 'External Id', fieldName: 'ExtId__c', type: 'number', sortable: true },
-        { label: 'Name', fieldName: 'Name', type: 'button', sortable: true,
-            typeAttributes: {label: { fieldName: 'Name' }, name:'view', target: '_self', variant: 'base'} },        
-        { label: 'Status', fieldName: 'Status__c', type: 'text' },
-        { label: 'Species', fieldName: 'Species__c', type: 'text' },
-        { label: 'Gender', fieldName: 'Gender__c', type: 'text' },
-        { label: 'Image URL', fieldName: 'ImageUrl__c', type: 'url' },
-        { label : 'Image', fieldName: 'ImageUrl__c', type: 'image', 
+        { label: 'External Id', fieldName: FIELD_EXTID.fieldApiName, type: 'number', sortable: true },
+        { label: 'Name', fieldName: FIELD_NAME.fieldApiName, type: 'button', sortable: true,
+            typeAttributes: {label: { fieldName: FIELD_NAME.fieldApiName }, name:'view', target: '_self', variant: 'base'} },        
+        { label: 'Status', fieldName: FIELD_STATUS.fieldApiName, type: 'text' },
+        { label: 'Species', fieldName: FIELD_SPECIES.fieldApiName, type: 'text' },
+        { label: 'Gender', fieldName: FIELD_GENDER.fieldApiName, type: 'text' },
+        { label: 'Image URL', fieldName: FIELD_IMAGE.fieldApiName, type: 'url' },
+        { label : 'Image', fieldName: FIELD_IMAGE.fieldApiName, type: 'image', 
             typeAttributes: {
                             url: { fieldName: 'ImageUrl__c' },
-                            name: { fieldName: 'Name' } 
+                            name: { fieldName: FIELD_NAME.fieldApiName } 
                             }
                         },
-        { label: 'URL', fieldName: 'Url__c', type: 'url' },
+        { label: 'URL', fieldName: FIELD_URL.fieldApiName, type: 'url' },
     ];
 
     @wire(getCharacters)    
